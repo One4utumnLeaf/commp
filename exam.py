00ae6603885b6d82c13d11fa0cc78c5ba0f18635
One4utumnLeaf <156811990+One4utumnLeaf@users.noreply.github.com>
2024-11-05 15:48:06 +0530

    Update exam.py

--- exam.py
+++ exam.py
@@ -12,6 +12,7 @@ with open('fund_database.csv', newline = '') as fund_data:
             reason = input("Reason for payment \n(Donation - 1) \n(Fees - 2) \n(Event Charges - 3) \n(Others - 4) \n>")
             pay = input("Payment Method: ")
             amt = input("Amount Payed: ")
+            ans = input("Do you want to add another record(y/n): ")
 def output():
     print("Welcome to Student Fund Management Sysytem")
     print("What operation do you want to perform")
